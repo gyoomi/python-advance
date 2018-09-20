b891be0716da3f1bd99ce22f9dadc8ce18e4fe0f
gyoomi <gyoomi0709@foxmail.com>
2018-09-20 18:08:17 +0800

    网络编程

---
+++ net/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : Leon
+# @Version : 2018/9/20 17:15
---
+++ net/updDemo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : Leon
+# @Version : 2018/9/20 17:15
+import socket
+# 创建一个TCP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 创建一个UDP socket
+s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
+
+
+
